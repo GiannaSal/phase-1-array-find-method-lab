a20bbc278f65ea5c1115324ef45d890c1498e1af
Salma Jediel <giannasal03@gmail.com>
2022-06-11 17:22:10 +0300

    commit tree 175db26cb91294e47c085bf2b03c0dbe04b09140

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code your solution here
+function superbowlWin(record){
+    for( const result of record){
+        if(record.result === "W"){
+            return(record.result);
+        }
+    }
+}
+
